dd1ac9ecd7729d0e248319a2f8c3858fe7a254b7
Uwe Riegel <uriegel@hotmail.de>
2020-03-15 21:58:25 +0100

    Project structure

---
+++ Tester/Program.fs
@@ -0,0 +1,23 @@
+﻿open Session
+open Request
+
+System.IO.Directory.SetCurrentDirectory "/media/speicher/projekte/UwebServer"
+
+printfn "Starting Test Server"
+
+let reitbeteiligungRequest = 
+    Request.useStatic "/media/speicher/projekte/UwebServer/webroot" "/Reitbeteiligung" 
+
+let testRequest = 
+    Request.useStatic "/media/speicher/projekte/UwebServer/webroot" "/test" 
+
+let configuration = Configuration.create {
+    Configuration.createEmpty() with 
+        Port = 9865
+        Requests = [ reitbeteiligungRequest; testRequest ]
+}
+
+let server = Server.create configuration 
+server.start ()
+stdin.ReadLine() |> ignore
+server.stop ()
