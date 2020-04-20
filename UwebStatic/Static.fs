1805699c0180ef3ef730eebdcda8da7d15f22235
Uwe Riegel <uriegel@hotmail.de>
2020-04-20 19:44:44 +0200

    Now dependency of UwebFiles

--- UwebStatic/Static.fs
+++ UwebStatic/Static.fs
@@ -1,22 +1,9 @@
 module Static
-open MimeTypes
 open System
 open System.IO
 open System.Text
 open ResponseData
 open Session
-open System.IO.Compression
-
-type QueryValues = {
-    query: string
-    url: string
-}
-
-type StaticInfo = {
-    localFile: string
-    redirUrl: string
-    isFile: bool
-}
 
 let private checkFile directory url = 
     let rawUrl = 
@@ -51,119 +38,6 @@ let private checkFile directory url =
         let file = Path.Combine (path, "index.html")
         if File.Exists file then file else ""
 
-let asyncSendRange file responseData = async {
-    ()
-}
-
-let asyncSendStream responseData (stream: Stream) (contentType: string) lastModified = 
-    async {
-        let mutable headers = Map.empty
-        let mutable streamToSend = stream
-        if responseData.requestData.header.contentEncoding.Value <> ContentEncoding.None &&
-            (contentType.StartsWith ("application/javascript", StringComparison.CurrentCultureIgnoreCase)            
-            || contentType.StartsWith ("text/", StringComparison.CurrentCultureIgnoreCase)) then
-            
-            let ms = new MemoryStream ()
-            use compressedStream = 
-                match responseData.requestData.header.contentEncoding.Value with    
-                | ContentEncoding.Deflate ->
-                    headers <- headers.Add("Content-Encoding", "deflate") 
-                    new DeflateStream (ms, CompressionMode.Compress, true) :> Stream
-                | ContentEncoding.GZip ->
-                    headers <- headers.Add("Content-Encoding", "gzip")
-                    new GZipStream (ms, CompressionMode.Compress, true) :> Stream
-                | _ -> null
-
-            do! stream.CopyToAsync compressedStream |> Async.AwaitTask
-            compressedStream.Close();
-            ms.Position <- 0L 
-            
-            streamToSend <- ms
-
-        if lastModified <> "" then headers <- headers.Add("Last-Modified", lastModified)     
-        // TODO:
-        let noCache = false
-        if noCache then
-            headers <- headers.Add("Cache-Control", "no-cache,no-store")
-            headers <- headers.Add("Expires", (DateTime.Now.Subtract(TimeSpan(1, 0, 0))).ToUniversalTime().ToString "r")   
-
-        headers <- ResponseHeaders.initialize headers contentType (Some (int streamToSend.Length))
-
-        if contentType.StartsWith ("application/javascript", StringComparison.CurrentCultureIgnoreCase) 
-            || contentType.StartsWith ("text/css", StringComparison.CurrentCultureIgnoreCase)
-            || contentType.StartsWith ("text/html", StringComparison.CurrentCultureIgnoreCase) then
-                headers <- headers.Add("Expires", DateTime.Now.ToUniversalTime().ToString "r")
-
-        let headerBytes = Response.createHeaderOk responseData headers None
-        do! responseData.requestData.session.networkStream.AsyncWrite (headerBytes, 0, headerBytes.Length)
-        
-        if responseData.requestData.header.method <> Method.Head then
-            let bytes = Array.zeroCreate 8192
-
-            let mutable dataToSend = true
-            while dataToSend do 
-                let! read = streamToSend.AsyncRead (bytes, 0, bytes.Length)
-                if read <> 0 then
-                    do! responseData.requestData.session.networkStream.AsyncWrite (bytes, 0, read)
-                else
-                    dataToSend <- false
-    }
-
-let asyncInternalSendFile file (responseData: ResponseData) = async {
-    let fi = FileInfo file
-
-    //let noCache = Server.Configuration.NoCacheFiles.Contains(file.ToLower());
-    let noCache = false
-    if not noCache then
-        // TODO: AppCache
-        ()
-
-    let isModifiedSince = 
-        match responseData.requestData.header.Header "If-Modified-Since" with
-        | None -> None
-        | Some a when a = "" -> None
-        | Some v -> 
-            let pos = v.IndexOf ";"
-            Some (if pos <> -1 then v.Substring (0, pos) else v)
-        
-    let modified =  
-        match isModifiedSince with
-        | Some v ->
-            let ifModifiedSince = Convert.ToDateTime v
-            let fileTime = fi.LastWriteTime.AddTicks -(fi.LastWriteTime.Ticks % TimeSpan.FromSeconds(1.0).Ticks)
-            let diff = fileTime - ifModifiedSince
-            diff > TimeSpan.FromMilliseconds 0.0
-        | None -> true
-
-    if modified then
-        let contentType = 
-            match fi.Extension with
-            | ".html" 
-            | ".htm" -> "text/html; charset=UTF-8"
-            | ".css" -> "text/css; charset=UTF-8"
-            | ".js" -> "application/javascript; charset=UTF-8"
-            | ".appcache" -> "text/cache-manifest"
-            | _ ->  getMimeType fi.Extension
-       
-        let dateTime = fi.LastWriteTime
-        let lastModified = dateTime.ToUniversalTime().ToString "r"
-
-        use stream = File.OpenRead file
-        do! asyncSendStream responseData stream contentType lastModified
-    else
-        do! Response.asyncSend304 responseData
-}
-
-let asyncSendFile (file: string) responseData = async {
-    if file.EndsWith (".mp4", StringComparison.InvariantCultureIgnoreCase)
-        || file.EndsWith (".mkv", StringComparison.InvariantCultureIgnoreCase)
-        || file.EndsWith (".mp3", StringComparison.InvariantCultureIgnoreCase)
-        || file.EndsWith (".wav", StringComparison.InvariantCultureIgnoreCase) then
-        do! asyncSendRange file responseData
-    else
-        do! asyncInternalSendFile file responseData
-}
-
 let asyncRedirectDirectory directory url responseData = async {
     let path = checkFile directory url
     if path = "" then
@@ -184,7 +58,7 @@ let rec asyncServeStaticUrl directory requestData url = async {
     let responseData = create requestData
     let file = checkFile directory url
     if file <> "" then  
-        do! asyncSendFile file responseData
+        do! Files.asyncSendFile file responseData
     elif not (url.EndsWith "/") then
         do! asyncRedirectDirectory directory (url + "/") responseData
     else
@@ -208,7 +82,7 @@ let private asyncServeFavicon iconPath (requestSession: RequestSession) = async 
         let requestData = requestSession.RequestData :?> RequestData.RequestData
         let responseData = create requestData
         if File.Exists iconPath then
-            do! asyncSendFile iconPath responseData
+            do! Files.asyncSendFile iconPath responseData
         else
             do! Response.asyncSendNotFound responseData
         return true
