3e08ff14e3fbc63ae148776077b88077ec22c127
Uwe Riegel <uriegel@hotmail.de>
2020-05-05 15:49:36 +0200

    Now using ranges

--- Tester/Program.fs
+++ Tester/Program.fs
@@ -8,13 +8,16 @@ printfn "Starting Test Server"
 let reitbeteiligungRequest = 
     Static.useStatic "/media/speicher/projekte/UwebServer/webroot" "/Reitbeteiligung" 
 
+let rangetest = 
+    Static.useStatic "/media/speicher/projekte/UwebStatic/" "/Starbuzz" 
+
 let testRequest = 
     Static.useStatic "/media/speicher/projekte/UwebServer/webroot" "/test" 
 
 let configuration = Configuration.create {
     Configuration.createEmpty() with 
         Port = 9865
-        Requests = [ reitbeteiligungRequest; testRequest ]
+        Requests = [ reitbeteiligungRequest; testRequest; rangetest ]
 }
 
 let server = Server.create configuration 
